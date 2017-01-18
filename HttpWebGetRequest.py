71973d3ae62d7a82fd2f65f243109272cfd048b0
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-01-18 14:16:40 -0800

    Change url prefix test from http://www. to http://

--- HttpWebGetRequest.py
+++ HttpWebGetRequest.py
@@ -1,11 +1,11 @@
 import requests
 def get_data(url_address):
     url_test=url_address[0:11]
-    if url_test.lower=="http://www.".lower:
+    if url_test.lower=="http://".lower:
         data = requests.get(url_address)
         return data
     else:
-        print("Check your url and ensure it starts with http://www.")
+        print("Check your url and ensure it starts with http://")
 
 print("\t\t\tMAKE URL GET REQUESTS FROM HERE\n")
 address=str(input("Please enter the url address you wish to get data from : "))
