613a0794b5aded224811004945e71d49b199d4b5
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-01-19 09:12:55 -0800

    Add factorial function

---
+++ Factorial.py
@@ -0,0 +1,8 @@
+"""Find the factorial of number e.g factorial(5) = 5*4*3*2*1 = 120
+def factorial(number):
+    factorial_of_number=number
+    number= number-1
+    while number>0:
+        factorial_of_number=factorial_of_number*number
+        number-=1
+    return factorial_of_number
